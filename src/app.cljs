31ee3d6db728b74cf7658fba80c92bd4d2a61f68
NickyTB <nicberggren@gmail.com>
2014-12-18 18:09:25 +0100

    Alternate way to render the todo list. Added delete functionality.
    Mostly copied from another tutorial and adjusted to todos.
    Intresting to see two ways to handle todo list rendering.

--- src/app.cljs
+++ src/app.cljs
@@ -17,23 +17,56 @@
 
 (defn add-todo [app owner]
   (let [new-field (om/get-node owner "new-todo")]
-    (do 
+    (do
       (om/transact! app :todos #(conj % {:id (rand-int 1000) :text (.-value new-field)}))
       (println @app-state))))
 
-(defn cljslab-app [{:keys [todos] :as app} owner]
-  (reify
-    om/IRender
-    (render [_]
-      (html [:div "Hello world!"
-              [:ul (for [todo todos]
-                [:li 
-                  {:key (:id todo)} 
-                  (:text todo)])]
-              [:input {:type "text" :ref "new-todo"}]
-              (html/submit-button 
-                {:on-click (partial add-todo app owner)} 
-                "React!")]))))
+;;(defn cljslab-app [{:keys [todos] :as app} owner]
+;;  (reify
+;;    om/IRender
+;;    (render [_]
+;;      (html [:div "Hello world!"
+;;              [:ul (for [todo todos]
+;;                [:li
+;;                  {:key (:id todo)}
+;;                  (:text todo)])]
+;;              [:input {:type "text" :ref "new-todo"}]
+;;              (html/submit-button
+;;                {:on-click (partial add-todo app owner)}
+;;                "React!")]))))
 
-(om/root cljslab-app app-state
+
+(defn todo-view [todo owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [delete]}]
+      (dom/li nil
+        (:text todo)
+        (dom/button #js {:onClick (fn [e] (put! delete @todo))} "Delete")))))
+
+(defn todos-view [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:delete (chan)})
+    om/IWillMount
+    (will-mount [_]
+      (let [delete (om/get-state owner :delete)]
+        (go (loop []
+          (let [todo (<! delete)]
+            (om/transact! app :todos
+              (fn [xs] (vec (remove #(= todo %) xs))))
+            (recur))))))
+    om/IRenderState
+    (render-state [this {:keys [delete]}]
+      (dom/div nil
+        (dom/h2 nil "Todo list")
+        (apply dom/ul nil
+          (om/build-all todo-view (:todos app)
+              {:init-state {:delete delete}}))
+                (dom/div nil
+                  (dom/input #js {:type "text" :ref "new-todo"})
+                  (dom/button #js {:onClick #(add-todo app owner)} "Add todo")) ))))
+
+(om/root todos-view  app-state
   {:target (.querySelector js/document "body")})
